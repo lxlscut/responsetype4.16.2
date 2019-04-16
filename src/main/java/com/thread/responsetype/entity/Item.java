5bf883596c5ee22b203af255bad744ed1fa26925
PC <2684017046@qq.com>
2019-04-16 10:38:38 +0800

    秒杀练习2 用户注册与登录练习

---
+++ src/main/java/com/thread/responsetype/entity/Item.java
@@ -0,0 +1,63 @@
+package com.thread.responsetype.entity;
+
+public class Item {
+    private Integer id;
+
+    private String title;
+
+    private Double price;
+
+    private String url;
+
+    private String description;
+
+    private Integer sale;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getSale() {
+        return sale;
+    }
+
+    public void setSale(Integer sale) {
+        this.sale = sale;
+    }
+}
